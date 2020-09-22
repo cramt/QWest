b5b397661a86231dee94b79812365a55896e5a9c
Alexandra Østermark <alex.cramt@gmail.com>
2020-09-22 13:00:30 +0200

    better scripting

--- QWest/App_Start/BundleConfig.cs
+++ QWest/App_Start/BundleConfig.cs
@@ -1,21 +1,44 @@
-﻿using System.Web;
+﻿using Microsoft.Ajax.Utilities;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Web;
 using System.Web.Optimization;
 
 namespace QWest {
     public class BundleConfig {
-        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles) {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            var scriptPath = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "scripts"));
+            var scriptsPath = Directory.GetDirectories(scriptPath);
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            Task.WaitAll(scriptsPath.Select(path => {
+                return Task.Factory.StartNew(() => {
+                    //npx webpack --config webpack.config.js
+                    Process process = new Process {
+                        StartInfo = new ProcessStartInfo {
+                            WorkingDirectory = path,
+                            RedirectStandardInput = true,
+                            RedirectStandardOutput = true,
+                            FileName = @"C:\Windows\System32\cmd.exe",
+                            Verb = "runas",
+                            Arguments = "/c npx webpack --config webpack.config.js",
+                            CreateNoWindow = true,
+                            UseShellExecute = false
+                        }
+                    };
+                    process.Start();
+                    process.WaitForExit();
+                    Debug.WriteLine(process.StandardOutput.ReadToEnd());
+                });
+            }).ToArray());
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
-
+            var scriptsName = scriptsPath.Select(x => Path.GetFileName(x));
+            scriptsName.ForEach(x => {
+                bundles.Add(new ScriptBundle("~/scripts/" + x).Include("~/scripts/" + x + "/dist/main.js"));
+            });
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
