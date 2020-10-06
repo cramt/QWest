15040dd81fd83086858a00bf40f3f9756f43e4a4
Alexandra Østermark <alex.cramt@gmail.com>
2020-10-07 01:17:46 +0200

    updated progressmap to keep track of its own changes

--- Model/ProgressMap.cs
+++ Model/ProgressMap.cs
@@ -1,23 +1,56 @@
 ﻿using GeographicSubdivision.Provider;
 using Newtonsoft.Json;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 
 namespace Model {
     public class ProgressMap {
+
         public int? Id { get; set; }
-        public List<string> Locations { get; set; }
+
+        public Collection<string> Locations { get; }
+
         [JsonIgnore]
         public IEnumerable<IReadOnlyCollection<AbstractLocation>> LocationsTransformed {
             get => Locations.Select(x => GeographyProvider.Instance.Alpha2Map[x]);
         }
+
+        [JsonIgnore]
+        public List<string> Additions { get; } = new List<string>();
+
+        [JsonIgnore]
+        public List<string> Subtractions { get; } = new List<string>();
+
         public ProgressMap() : this(new List<string>()) {
 
         }
         public ProgressMap(List<string> locations) : this(locations, null) {
 
         }
-        public ProgressMap(List<string> locations, int? id) {
+        public ProgressMap(List<string> locations, int? id) : this(new ObservableCollection<string>(locations), id) {
+
+        }
+
+        public ProgressMap(ObservableCollection<string> locations, int? id) {
+            locations.CollectionChanged += (o, e) => {
+                foreach(string item in e.NewItems) {
+                    if (Subtractions.Contains(item)) {
+                        Subtractions.Remove(item);
+                    }
+                    else {
+                        Additions.Add(item);
+                    }
+                }
+                foreach (string item in e.OldItems) {
+                    if (Additions.Contains(item)) {
+                        Additions.Remove(item);
+                    }
+                    else {
+                        Subtractions.Add(item);
+                    }
+                }
+            };
             Locations = locations;
             Id = id;
         }
--- QWest.DataAccess/DAOProgressMap.cs
+++ QWest.DataAccess/DAOProgressMap.cs
@@ -28,55 +28,48 @@ namespace QWest.DataAcess {
             public static async Task<RProgressMap> Get(int id) {
                 SqlCommand stmt = ConnectionWrapper.CreateCommand("select location from progress_maps inner join progress_maps_locations on progress_maps.id = progress_maps_locations.progress_maps_id where progress_maps.id = @id");
                 stmt.Parameters.AddWithValue("@id", id);
-                return new RProgressMap {
-                    Locations = (await stmt.ExecuteReaderAsync()).ToIterator(reader => reader.GetSqlString(0).Value).ToList(),
-                    Id = id
-                };
+                return new RProgressMap((await stmt.ExecuteReaderAsync()).ToIterator(reader => reader.GetSqlString(0).Value).ToList(), id);
             }
             public static async Task<RProgressMap> GetByUserId(int userId) {
                 SqlCommand stmt = ConnectionWrapper.CreateCommand("select progress_maps.id, location from users inner join progress_maps on users.progress_maps_id = progress_maps.id inner join progress_maps_locations on progress_maps.id = progress_maps_locations.progress_maps_id where users.id = @id");
                 stmt.Parameters.AddWithValue("@id", userId);
-                RProgressMap map = new RProgressMap();
                 List<(int, string)> result = (await stmt.ExecuteReaderAsync()).ToIterator(reader => (reader.GetSqlInt32(0).Value, reader.GetSqlString(1).Value)).ToList();
                 if (result.Count == 0) {
                     stmt = ConnectionWrapper.CreateCommand("select progress_maps.id from users inner join progress_maps on users.progress_maps_id = progress_maps.id where users.id = @id");
                     stmt.Parameters.AddWithValue("@id", userId);
-                    map.Id = (await stmt.ExecuteReaderAsync()).ToIterator(reader => reader.GetSqlInt32(0).Value).First();
-                    return map;
+                    return new RProgressMap(new List<string>(), (await stmt.ExecuteReaderAsync()).ToIterator(reader => reader.GetSqlInt32(0).Value).First());
                 }
                 else {
-                    map.Id = result[0].Item1;
-                    map.Locations = result.Select(x => x.Item2).ToList();
-                    return map;
+                    return new RProgressMap(result.Select(x => x.Item2).ToList(), result[0].Item1);
                 }
             }
-            public static async Task Update(RProgressMap newMap) {
-                RProgressMap existingMap = await Get(newMap);
-                ListDifferenceResult<string> diffs = ListDifference(existingMap.Locations, newMap.Locations);
-                if (diffs.Count == 0) {
+            public static async Task Update(RProgressMap map) {
+                if ((map.Additions.Count + map.Subtractions.Count) == 0) {
                     return;
                 }
                 string insertSql;
                 string deleteSql;
-                if (diffs.Additions.Count == 0) {
+                if (map.Additions.Count == 0) {
                     insertSql = "";
                 }
                 else {
                     insertSql = "INSERT INTO progress_maps_locations (progress_maps_id, location) VALUES "
-                       + string.Join(",", diffs.Additions.Select((_, i) => "(@id, @add_location" + i + ")").ToArray()) + ";";
+                       + string.Join(",", map.Additions.Select((_, i) => "(@id, @add_location" + i + ")").ToArray()) + ";";
                 }
-                if (diffs.Subtractions.Count == 0) {
+                if (map.Subtractions.Count == 0) {
                     deleteSql = "";
                 }
                 else {
                     deleteSql = "DELETE FROM progress_maps_locations WHERE progress_maps_id = @id AND ("
-                    + string.Join(" OR ", diffs.Subtractions.Select((_, i) => "location = @sub_location" + i).ToArray()) + ")";
+                    + string.Join(" OR ", map.Subtractions.Select((_, i) => "location = @sub_location" + i).ToArray()) + ")";
                 }
                 SqlCommand stmt = ConnectionWrapper.CreateCommand(insertSql + deleteSql);
-                stmt.Parameters.AddWithValue("@id", existingMap.Id);
-                diffs.Additions.Select((x, i) => stmt.Parameters.AddWithValue("@add_location" + i, x));
-                diffs.Subtractions.Select((x, i) => stmt.Parameters.AddWithValue("@sub_location" + i, x));
+                stmt.Parameters.AddWithValue("@id", map.Id);
+                map.Additions.Select((x, i) => stmt.Parameters.AddWithValue("@add_location" + i, x));
+                map.Subtractions.Select((x, i) => stmt.Parameters.AddWithValue("@sub_location" + i, x));
                 await stmt.ExecuteNonQueryAsync();
+                map.Additions.Clear();
+                map.Subtractions.Clear();
             }
         }
     }
