0fc2eb4554761072a7ad40cef40621fdbfd79974
Alexandra Østermark <alex.cramt@gmail.com>
2020-10-07 13:27:11 +0200

    first part of map editing done

--- QWest.Web/frontend/map/index.js
+++ QWest.Web/frontend/map/index.js
@@ -30,36 +30,16 @@ async function fetchData() {
     return { userData, staticData }
 }
 
-function getVisitationPercentage(subdividable) {
-    let subdivisions = subdividable.subdivision;
+const getVisitationPercentage = (subdividable) => {
+    let subdivisions = Object.values(subdividable.subdivision);
     if (subdivisions.length == 0) {
         return subdividable.visited ? 1 : 0
     }
     return subdivisions.map(getVisitationPercentage).reduce((a, b) => a + b, 0) / subdivisions.length
 }
 
-$(async () => {
-    let { userData, staticData } = await fetchData();
-    userData.progressMap.locations = userData.progressMap.locations.map(x => x.split("-"))
-    staticData = staticData.reduce((acc, x) => {
-        acc[x.alpha_2] = x
-        return acc
-    }, {})
-    userData.progressMap.locations.forEach(location => {
-        let country = staticData[location[0]]
-        if (location.length == 1) {
-            country.visited = true
-        }
-        else {
-            let curr = country;
-            for (let i = 1; i < location.length; i++) {
-                curr = curr.subdivision.find(x => x.code == location[i])
-            }
-            curr.visited = true
-        }
-    })
-    staticData = Object.values(staticData)
-    let mapData = staticData.reduce((acc, x) => {
+const renderMap = (staticData) => {
+    let mapData = Object.values(staticData).reduce((acc, x) => {
         let visited = getVisitationPercentage(x)
         acc[x.alpha_2] = {
             visited: visited * 100
@@ -81,4 +61,54 @@ $(async () => {
             values: mapData
         }
     });
+    return map
+}
+
+const mapOutStaticData = (staticData, prop = "alpha_2") => {
+    if (staticData.length === 0) {
+        return []
+    }
+    return staticData.reduce((acc, x) => {
+        acc[x[prop]] = x
+        x.subdivision = mapOutStaticData(x.subdivision, "code")
+        return acc
+    }, {})
+}
+
+$(async () => {
+    let { userData, staticData } = await fetchData();
+    userData.progressMap.locations.push("DK-81")
+    userData.progressMap.locations = userData.progressMap.locations.map(x => x.split("-"))
+    let staticDataMap = mapOutStaticData(staticData)
+    userData.progressMap.locations.forEach(location => {
+        let country = staticDataMap[location[0]]
+        if (location.length == 1) {
+            country.visited = true
+        }
+        else {
+            let curr = country;
+            for (let i = 1; i < location.length; i++) {
+                curr = curr.subdivision[location[i]]
+            }
+            curr.visited = true
+        }
+    })
+
+    renderMap(staticDataMap)
+
+    let customMenu = $(".custom-menu")
+    $(".svgMap-country").on("contextmenu", e => {
+        e.preventDefault();
+        let alpha2 = e.currentTarget.getAttribute("data-id")
+        customMenu.empty()
+        const edit = $("<li>Edit " + staticDataMap[alpha2].name + "</li>")
+        customMenu.append(edit)
+        edit.on("click", () => {
+            window.location.href = "/edit_map.html?alpha_2=" + alpha2
+        })
+        customMenu.finish().toggle(100).css({
+            top: e.pageY + "px",
+            left: e.pageX + "px"
+        })
+    })
 })
