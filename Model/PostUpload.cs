6b095d6b404c7e9218cce8a190249c942c53dc9f
Alexandra Østermark <alex.cramt@gmail.com>
2020-10-15 10:54:31 +0200

    post support

---
+++ Model/PostUpload.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using Utilities;
+
+namespace Model {
+    public class PostUpload {
+        public int? Id { get; set; }
+        public string Contents { get; set; }
+        [JsonIgnore]
+        public User User { get; set; }
+        public DateTime PostTime { get; set; }
+        [JsonIgnore]
+        public List<byte[]> Images { get; set; }
+        public string LocationId { get; set; }
+        public PostUpload(string contents, User user, int PostUploadTime, List<byte[]> images)
+            : this(contents, user, PostUploadTime, images, null) {
+
+        }
+        public PostUpload(string contents, User user, int PostUploadTime, List<byte[]> images, int? id)
+            : this(contents, user, unchecked((uint)PostUploadTime), images, null, id) {
+
+        }
+        public PostUpload(string contents, User user, uint PostUploadTime, List<byte[]> images)
+            : this(contents, user, PostUploadTime, images, null) {
+        }
+        public PostUpload(string contents, User user, uint PostUploadTime, List<byte[]> images, string locationId)
+            : this(contents, user, PostUploadTime, images, locationId, null) {
+        }
+        public PostUpload(string contents, User user, uint PostUploadTime, List<byte[]> images, string locationId, int? id)
+            : this(contents, user, Config.Config.Instance.StartDate.AddSeconds(PostUploadTime), images, locationId, id) {
+        }
+        public PostUpload(string contents, User user, DateTime PostUploadTime, List<byte[]> images)
+            : this(contents, user, PostUploadTime, images, null) {
+
+        }
+
+        public PostUpload(string contents, User user, DateTime PostUploadTime, List<byte[]> images, string locationId)
+            : this(contents, user, PostUploadTime, images, locationId, null) {
+
+        }
+        public PostUpload(string contents, User user, DateTime PostUploadTime, List<byte[]> images, string locationId, int? id) {
+            Contents = contents;
+            User = user;
+            PostUploadTime = PostUploadTime;
+            Images = images;
+            Id = id;
+            LocationId = locationId;
+        }
+    }
+}
---
+++ QWest.Api/Controllers/PostController.cs
@@ -0,0 +1,48 @@
+﻿using Model;
+using QWest.DataAcess;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using static Utilities.Utilities;
+
+namespace QWest.Apis {
+    public class PostController : ApiController {
+        [HttpPost]
+        [ResponseType(typeof(Post))]
+        public async Task<HttpResponseMessage> Upload([FromBody] PostUpload upload) {
+            User user = Request.GetOwinContext().Get<User>("user");
+            if (user == null) {
+                return new HttpResponseMessage(HttpStatusCode.Unauthorized);
+            }
+            upload.User = user;
+            if (Request.Content.IsMimeMultipartContent()) {
+                var provider = new MultipartMemoryStreamProvider();
+                await Request.Content.ReadAsMultipartAsync(provider);
+                upload.Images = (await Task.WhenAll(provider.Contents.Select(file => {
+                    //TODO: check for mine type
+                    return file.ReadAsByteArrayAsync();
+                }))).ToList();
+            }
+            else {
+                upload.Images = new List<byte[]>();
+            }
+            Post post = await DAO.Post.Add(upload);
+            return Request.CreateResponse(HttpStatusCode.OK, post);
+        }
+        [ResponseType(typeof(List<Post>))]
+        public async Task<HttpResponseMessage> GetUsersPosts(int? id) {
+            if(id == null) {
+                User user = Request.GetOwinContext().Get<User>("user");
+                if (user == null) {
+                    return new HttpResponseMessage(HttpStatusCode.Unauthorized);
+                }
+                id = user.Id;
+            }
+            return Request.CreateResponse(HttpStatusCode.OK, await DAO.Post.GetByUserId(id ?? 0));
+        }
+    }
+}
