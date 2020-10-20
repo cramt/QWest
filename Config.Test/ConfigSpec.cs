c60925a3552bddbe5ec5c79160563b744a4abb9d
Alexandra Østermark <alex.cramt@gmail.com>
2020-10-20 23:45:25 +0200

    more test stuff

---
+++ Config.Test/ConfigSpec.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace Config.Test {
+    [TestFixture]
+    public class ConfigSpec {
+        [Test]
+        public void NotNull() {
+            Assert.NotNull(Config.Instance);
+            Assert.NotNull(Config.Instance.ApiPort);
+            Assert.NotNull(Config.Instance.DatabaseConnectionString);
+            Assert.NotNull(Config.Instance.EmailPort);
+            Assert.NotNull(Config.Instance.Ports);
+            Assert.NotNull(Config.Instance.ServePort);
+            Assert.NotNull(Config.Instance.StartDate);
+            Assert.NotNull(Config.Instance.StartYear);
+        }
+    }
+}
