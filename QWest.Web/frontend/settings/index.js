ec164b2d0268d724611204512dea25a1d9f086dc
penismusic <1080615@ucn.dk>
2020-10-23 18:12:14 +0200

    pretty settings page

--- QWest.Web/frontend/settings/index.js
+++ QWest.Web/frontend/settings/index.js
@@ -41,7 +41,7 @@ $(async () => {
 
     descriptionField.text(user.description)
 
-    profilePictureContainer.append("<img src='/api/Image/Get?id=" + user.profilePicture + "' />")
+    profilePictureContainer.append("<img id=" + "image" + " width=" + "500px" + " src='/api/Image/Get?id=" + user.profilePicture + "' />")
 
     const profilePictureUpdate = async () => {
         const files = profilePictureField[0].files;
