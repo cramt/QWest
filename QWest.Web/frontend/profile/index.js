471b79c352ee15d74c94919ea604a6d4691eb1f4
m0rxx <sravn18@student.aau.dk>
2020-10-29 13:02:06 +0100

    tried adding profile pic to profile view

--- QWest.Web/frontend/profile/index.js
+++ QWest.Web/frontend/profile/index.js
@@ -1,15 +1,26 @@
 import $ from "jquery";
 import "cookie-store"
+import { fetchLogedInUser } from "../whoami"
 
+const userPromise = fetchLogedInUser();
 
 $(async () => {
+    const user = await userPromise;
+    const logoutButton = $("#logout-button")
+    const usernameField = $("#username-field")
+    const descriptionField = $("#description-field");
+    const profilePictureContainer = $("#profile-picture-container")
     let logoutButton = $("#logout-button")
-    let userSettings = $("#user-settings")
 
     logoutButton.on("click", async() => {
         await cookieStore.delete("sessionCookie")
         window.location.href = "/login.html"
     })
     
+    usernameField.val(user.username)
+
+    descriptionField.text(user.description)
+
+    profilePictureContainer.append("<img id=" + "image" + " width=" + "500px" + " src='/api/Image/Get?id=" + user.profilePicture + "' />")
     
 })
