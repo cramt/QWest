d3ff59d9c8d0f3e08824d4018c7f105201bc4219
penismusic <1080615@ucn.dk>
2020-11-11 12:09:36 +0100

    add friend button fix?

--- QWest.Web/frontend/profile/index.js
+++ QWest.Web/frontend/profile/index.js
@@ -26,9 +26,9 @@ $(async () => {
     profilePictureContainer.append('<img id="image" width="500px" src="/api/Image/Get?id=' + user.profilePicture + '" />')
 
     if (!isMe) {
+        var us = document.getElementById("#user-settings")
         userSettings.text("Add friend")
-        userSettings.removeAttr("href")
-        userSettings.href("#")
+        us.setAttribute("href", "#")
         userSettings.on("click", async () => {
             const request = await fetch("/api/Friendship/AddFriend?id=" + user.id, {
                 method: "POST",
