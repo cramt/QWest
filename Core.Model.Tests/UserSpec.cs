53ee5465525de6d67edac878be30c05bee6fd946
Alexandra Østermark <alex.cramt@gmail.com>
2020-12-21 11:29:59 +0100

    Model tests have been ported

---
+++ Core.Model.Tests/UserSpec.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+namespace Model.Tests {
+    [TestFixture]
+    public class UserTest {
+        [Test]
+        public void PasswordVerification() {
+            var user = new User("test user", "12345678", "email");
+            Assert.True(user.VerifyPassword("12345678"));
+        }
+    }
+}
